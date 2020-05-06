6d82b6842224d1c729fd06d219734836a5ca0ffa
Sobira <sobira204@gmail.com>
2020-05-06 17:51:14 +0500

    add method getAll

--- MyList.cs
+++ MyList.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace Collection
 {
     public class MyList<T>
@@ -6,6 +7,11 @@ namespace Collection
             list = new T[0];
         }
         public T[] list;
+        public int Length{
+            get{
+                return list.Length;
+            }
+        }
         public void Add(T item){
             T[] nList = new T[list.Length + 1];
             for(int i = 0; i < list.Length; i++)
@@ -16,6 +22,15 @@ namespace Collection
         public T GetElement(int index){
             return list[index];
         }
-        
+        public IEnumerable<T> GetAll(int max){
+            for (int i = 0; i < max; i++){
+                if(i == list.Length){
+                    yield break;
+                }
+                else{
+                    yield return list[i];
+                }
+            }
+        }        
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,11 @@ namespace Collection
         {
             MyList<int> n = new MyList<int>();
             n.Add(5);
-            System.Console.WriteLine(n.list[0]);
+            System.Console.WriteLine(n.Length);
+            n.Add(8);
+            n.Add(9);
+            foreach(var m in n.GetAll(20))
+            System.Console.WriteLine(m);
         }
     }
 }
