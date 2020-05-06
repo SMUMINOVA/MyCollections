7f2781b2a5f5d11a277758bec3f481ab139fabd2
Sobira <sobira204@gmail.com>
2020-05-06 15:58:43 +0500

    create class MyList and add method

---
+++ MyList.cs
@@ -0,0 +1,17 @@
+namespace Collection
+{
+    public class MyList<T>
+    {
+        public MyList(){
+            list = new T[0];
+        }
+        public T[] list;
+        public void Add(T item){
+            T[] nList = new T[list.Length + 1];
+            for(int i = 0; i < list.Length; i++)
+                nList[i] = list[i];
+            nList[list.Length] = item;
+            list = nList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> n = new MyList<int>();
+            n.Add(5);
+            System.Console.WriteLine(n.list[0]);
+        }
+    }
+}
